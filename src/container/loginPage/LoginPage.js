4d77d94af6df8a9e278d0430dc05b7b42fde7f0b
ccfine <469781947@qq.com>
2018-05-16 18:31:12 +0800

    erpTV-RN

--- src/container/loginPage/LoginPage.js
+++ src/container/loginPage/LoginPage.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react"
-import { StyleSheet, View, Text, ImageBackground } from "react-native"
+import { StyleSheet, View, Text, ImageBackground, TextInput } from "react-native"
 
 export default class LoginPage extends Component {
+  constructor () {
+    super()
+    this.state = {
+      pwd: ""
+    }
+  }
+  handleChangePwd (value) {
+    this.setState({
+      pwd: value,
+    })
+  }
   render () {
     return (    
-      <ImageBackground source={ require("./bg.png") } style={ styles.backgroundImage } resizeMode="cover">
+      <ImageBackground source={ require("./bg.png") } resizeMode="cover" style={ styles.bgImage }>
         <View style={ styles.container }>
           <Text style={ styles.title }>
             <Text style={ styles.xq }>【小强</Text>
@@ -13,6 +24,23 @@ export default class LoginPage extends Component {
             <Text style={ styles.tv }>TV</Text>
             <Text style={ styles.ban }>版</Text>
           </Text>
+          <ImageBackground source={ require("./pwd.png") } resizeMode="cover" style={ styles.pwdImage }>
+            <View style={ styles.container }>
+              <TextInput value={ this.state.pwd } autoFocus={ true } maxLength={ 8 } placeholder="请输入统计序列号" placeholderTextColor="#34c3ff"
+                         underlineColorAndroid="transparent" style={ [styles.text, { width: 200 }] } onChangeText={ value => this.handleChangePwd(value) }
+              />
+            </View>
+          </ImageBackground>
+          <ImageBackground source={ require("./login.png") } resizeMode="cover" style={ styles.loginImage }>
+            <View style={ styles.container }>
+              <Text style={ styles.text }>登录</Text>
+            </View>
+          </ImageBackground>
+          <ImageBackground source={ require("./msg.png") } resizeMode="cover" style={ styles.msgImage }>
+            <View style={ styles.container }>
+              <Text style={ styles.msg }>您输入的系统序列号有误！</Text>
+            </View>
+          </ImageBackground>
         </View>
       </ImageBackground>
     )
@@ -20,7 +48,7 @@ export default class LoginPage extends Component {
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
+  bgImage: {
     width: 960,
     height: 540
   },
@@ -52,5 +80,28 @@ const styles = StyleSheet.create({
   ban: {
     fontSize: 70.5,
     color: "#ffe400"
+  },
+  pwdImage: {
+    width: 248.5,
+    height: 52,
+    marginTop: 23, 
+  },
+  loginImage: {
+    width: 148.5,
+    height: 52,
+    marginTop: 20
+  },
+  text: {
+    fontSize: 22,
+    color: "#34c3ff",
+  },
+  msgImage: {
+    width: 127.5,
+    height: 22,
+    marginTop: 35
+  },
+  msg: {
+    fontSize: 10,
+    color: "#34c3ff"
   }
 })
